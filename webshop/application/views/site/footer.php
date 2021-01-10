e9d3f151cd1f77fd8e4a1bcb78d960dc1822b75a
vanhai <minhaivan113@gmail.com>
2021-01-10 12:59:39 +0700

    commit
    hoàn thành

---
+++ webshop/application/views/site/footer.php
@@ -0,0 +1,21 @@
+<div class="row" >
+		</div>
+		<div class="row" id="footer">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+					<address>
+					  <strong>    SHOP Quần Áo 78 </strong><br>
+					  <span class="glyphicon glyphicon-home" aria-hidden="true"></span> Địa chỉ: 259 Trường Chinh - Phường 7 - TP.Tuy Hòa<br>
+					  <span class="glyphicon glyphicon-phone" aria-hidden="true"></span> Điện thoại: 0971539244
+					  <br>
+					  Copyright ©2020 - Design by 78
+					</address>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 text-right">
+					<a href="#"><img src="<?php echo base_url(); ?>upload/icon/facebook.png" alt=""></a>
+					<a href="#"><img src="<?php echo base_url(); ?>upload/icon/twitter.png" alt=""></a>
+					<a href="#"><img src="<?php echo base_url(); ?>upload/icon/google.png" alt=""></a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
---
+++ webshop/system/core/Controller.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class CI_Controller {
+
+	/**
+	 * Reference to the CI singleton
+	 *
+	 * @var	object
+	 */
+	private static $instance;
+
+	/**
+	 * Class constructor
+	 *
+	 * @return	void
+	 */
+	public function __construct()
+	{
+		self::$instance =& $this;
+
+		foreach (is_loaded() as $var => $class)
+		{
+			$this->$var =& load_class($class);
+		}
+
+		$this->load =& load_class('Loader', 'core');
+		$this->load->initialize();
+		log_message('info', 'Controller Class Initialized');
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Get the CI singleton
+	 *
+	 * @static
+	 * @return	object
+	 */
+	public static function &get_instance()
+	{
+		return self::$instance;
+	}
+
+}
